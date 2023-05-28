daa89f75be8c1d4a5cb17f20947708e6f692f881
camposa95 <95255953+camposa95@users.noreply.github.com>
2023-05-28 17:31:09 -0400

    twoSums

---
+++ src/main/java/TwoSums.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class TwoSums {
+    
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] solution = new int[2];
+
+        HashMap<Integer, Integer> numToIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int currentNum = nums[i];
+            int complement = target - currentNum;
+
+            if (numToIndex.containsKey(complement)) {
+                solution[0] = i;
+                solution[1] = numToIndex.get(complement);
+                break;
+            }
+
+            numToIndex.put(currentNum, i);
+        }
+
+
+        return solution;
+    }
+}
