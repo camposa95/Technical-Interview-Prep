bfcc46400c1dbca3ac79414af76dabce1bdb64f5
camposa95 <95255953+camposa95@users.noreply.github.com>
2023-05-28 18:03:21 -0400

    max profit

---
+++ src/main/java/MaxProfit.java
@@ -0,0 +1,18 @@
+public class MaxProfit {
+
+    public int maxProfit(int[] prices) {
+        int buyPrice = Integer.MAX_VALUE;
+        int Profit = 0;
+
+        int todaysPrice = Integer.MAX_VALUE;
+
+        for (int i = 0; i < prices.length; i++) {
+            todaysPrice = prices[i];
+            buyPrice = Math.min(buyPrice, todaysPrice); // either this will decrease or .. 
+
+            Profit = Math.max(Profit, todaysPrice - buyPrice); // this will
+        }
+
+        return Profit;
+    }
+}
