851f42a3152d74f7d02ec1db544022162596560d
camposa95 <95255953+camposa95@users.noreply.github.com>
2023-05-28 18:11:43 -0400

    containsDuplicate

---
+++ src/main/java/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> containedNums = new HashSet<>();
+        for (int num: nums) {
+            if (!containedNums.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
