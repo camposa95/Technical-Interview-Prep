adc668b9b4c48726ba95ef959f25335ce7cd9d3c
camposa95 <camposa@rose-hulman.edu>
2023-07-11 17:51:41 -0700

    added source

---
+++ src/main/java/TechnicalAssesments/Citadel/Citadel1.java
@@ -0,0 +1,20 @@
+package TechnicalAssesments.Citadel;
+
+public class Citadel1 {
+
+    /*
+     * Complete the 'findNumberOfWays' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     * 1. INTEGER n_intervals
+     * 2. INTEGER n_processes
+     */
+
+    public static int findNumberOfWays(int n_intervals, int n_processes) {
+        
+        
+        
+        return -1;
+    }
+}
