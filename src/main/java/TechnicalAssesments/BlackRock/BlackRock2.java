631060bbeed3ce39994892553efc091921702a1c
camposa95 <camposa@rose-hulman.edu>
2023-07-13 22:11:02 -0700

    vdfbf

--- src/main/java/TechnicalAssesments/BlackRock/BlackRock2.java
+++ src/main/java/TechnicalAssesments/BlackRock/BlackRock2.java
@@ -12,105 +12,105 @@ public class BlackRock2 {
     /**
    * Iterate through each line of input.
    */
-  public static void main(String[] args) throws IOException {
-    InputStreamReader reader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
-    BufferedReader in = new BufferedReader(reader);
-    String line;
+    public static void main(String[] args) throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
+        BufferedReader in = new BufferedReader(reader);
+        String line;
 
-    int lineNumber = 0;
-    String fxRates = null;
-    String selectedCurrency = null;
-    String targetCurrency = null;
+        int lineNumber = 0;
+        String fxRates = null;
+        String selectedCurrency = null;
+        String targetCurrency = null;
 
-    while ((line = in.readLine()) != null) {
-        if (lineNumber == 0) {
-          fxRates = line;
+        while ((line = in.readLine()) != null) {
+            if (lineNumber == 0) {
+            fxRates = line;
+            }
+        
+            if (lineNumber == 1) {
+            selectedCurrency = line;
+            }
+        
+            if (lineNumber == 2) {
+            targetCurrency = line;
+            }
+        
+            lineNumber++;
         }
-      
-        if (lineNumber == 1) {
-          selectedCurrency = line;
-        }
-      
-        if (lineNumber == 2) {
-          targetCurrency = line;
-        }
-      
-        lineNumber++;
+
+    // Process the fxRates and calculate the maximum amount of the target currency
+    double maxAmount = calculateMaxAmount(fxRates, selectedCurrency, targetCurrency);
+
+    // Print the result
+    System.out.println(maxAmount);
     }
 
-  // Process the fxRates and calculate the maximum amount of the target currency
-  double maxAmount = calculateMaxAmount(fxRates, selectedCurrency, targetCurrency);
+    private static double calculateMaxAmount(String fxRates, String selectedCurrency, String targetCurrency) {
 
-  // Print the result
-  System.out.println(maxAmount);
-}
+        class Pair<K, V> {
+            private K key;
+            private V value;
 
-private static double calculateMaxAmount(String fxRates, String selectedCurrency, String targetCurrency) {
+            public Pair(K key, V value) {
+                this.key = key;
+                this.value = value;
+            }
 
-    class Pair<K, V> {
-        private K key;
-        private V value;
+            public K getKey() {
+                return key;
+            }
 
-        public Pair(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-
-        public K getKey() {
-            return key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-    }
-
-    String[] rateList = fxRates.split(";");
-
-    // Create a map to store the exchange rates
-    Map<String, Map<String, Double>> exchangeRates = new HashMap<>();
-
-    // Populate the exchangeRates map with the provided rates
-    for (String rate : rateList) {
-        String[] parts = rate.split(",");
-        String fromCurrency = parts[0];
-        String toCurrency = parts[1];
-        double rateValue = Double.parseDouble(parts[2]);
-
-        // Add the exchange rate to the map
-        if (!exchangeRates.containsKey(fromCurrency)) {
-            exchangeRates.put(fromCurrency, new HashMap<>());
-        }
-        exchangeRates.get(fromCurrency).put(toCurrency, rateValue);
-    }
-
-    // Use Breadth-First Search (BFS) to find the maximum amount of the target currency
-    Queue<Pair<String, Double>> queue = new LinkedList<>();
-    queue.add(new Pair<>(selectedCurrency, 1.0));
-
-    while (!queue.isEmpty()) {
-        Pair<String, Double> pair = queue.poll();
-        String currentCurrency = pair.getKey();
-        double currentAmount = pair.getValue();
-
-        // If the current currency is the target currency, return the current amount
-        if (currentCurrency.equals(targetCurrency)) {
-            return currentAmount;
-        }
-
-        // Check if there are exchange rates for the current currency
-        if (exchangeRates.containsKey(currentCurrency)) {
-            Map<String, Double> rates = exchangeRates.get(currentCurrency);
-            for (String nextCurrency : rates.keySet()) {
-                double exchangeRate = rates.get(nextCurrency);
-                double nextAmount = currentAmount * exchangeRate;
-                queue.add(new Pair<>(nextCurrency, nextAmount));
+            public V getValue() {
+                return value;
             }
         }
-    }
 
-    // If the target currency cannot be reached, return -1.0
-    return -1.0;
-}
+        String[] rateList = fxRates.split(";");
+
+        // Create a map to store the exchange rates
+        Map<String, Map<String, Double>> exchangeRates = new HashMap<>();
+
+        // Populate the exchangeRates map with the provided rates
+        for (String rate : rateList) {
+            String[] parts = rate.split(",");
+            String fromCurrency = parts[0];
+            String toCurrency = parts[1];
+            double rateValue = Double.parseDouble(parts[2]);
+
+            // Add the exchange rate to the map
+            if (!exchangeRates.containsKey(fromCurrency)) {
+                exchangeRates.put(fromCurrency, new HashMap<>());
+            }
+            exchangeRates.get(fromCurrency).put(toCurrency, rateValue);
+        }
+
+        // Use Breadth-First Search (BFS) to find the maximum amount of the target currency
+        Queue<Pair<String, Double>> queue = new LinkedList<>();
+        queue.add(new Pair<>(selectedCurrency, 1.0));
+
+        while (!queue.isEmpty()) {
+            Pair<String, Double> pair = queue.poll();
+            String currentCurrency = pair.getKey();
+            double currentAmount = pair.getValue();
+
+            // If the current currency is the target currency, return the current amount
+            if (currentCurrency.equals(targetCurrency)) {
+                return currentAmount;
+            }
+
+            // Check if there are exchange rates for the current currency
+            if (exchangeRates.containsKey(currentCurrency)) {
+                Map<String, Double> rates = exchangeRates.get(currentCurrency);
+                for (String nextCurrency : rates.keySet()) {
+                    double exchangeRate = rates.get(nextCurrency);
+                    double nextAmount = currentAmount * exchangeRate;
+                    queue.add(new Pair<>(nextCurrency, nextAmount));
+                }
+            }
+        }
+
+        // If the target currency cannot be reached, return -1.0
+        return -1.0;
+    }
 
 }
