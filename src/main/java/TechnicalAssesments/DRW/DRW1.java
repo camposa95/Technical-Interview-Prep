556eff350fa70dd323e74601b7b78154abd9fb20
camposa95 <95255953+camposa95@users.noreply.github.com>
2023-07-20 18:41:49 -0700

    Update DRW1.java

--- src/main/java/TechnicalAssesments/DRW/DRW1.java
+++ src/main/java/TechnicalAssesments/DRW/DRW1.java
@@ -1,5 +1,21 @@
 package TechnicalAssesments.DRW;
 
 public class DRW1 {
-    
+    public int solution(int[] A) {
+        // Get the bitwise AND of all elements in the array
+        int result = A[0];
+        for (int i = 1; i < A.length; i++) {
+            result &= A[i];
+        }
+
+        // Count the number of trailing zeros in the bitwise AND result
+        int count = 0;
+        while (result % 2 == 0) {
+            result >>= 1;
+            count++;
+        }
+
+        // Return the size of the largest possible subset
+        return (int) Math.pow(2, count);
+    }
 }
