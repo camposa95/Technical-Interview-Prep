591b852b46fc65b900e93764cf0d551f6117096f
camposa95 <95255953+camposa95@users.noreply.github.com>
2023-08-05 12:17:21 -0700

    Update GM1.java

--- src/main/java/TechnicalAssesments/GM/GM1.java
+++ src/main/java/TechnicalAssesments/GM/GM1.java
@@ -4,25 +4,23 @@ public class GM1 {
     public int solution(int[] A) {
         int n = A.length;
         int count = 0;
+        boolean positive = true;
 
-        for (int i = 0; i < n; i += 2) {
-            if (A[i] <= 0) {
-                if (i + 1 < n && A[i + 1] == 0) {
-                    return -1; // If there's a 0 after a non-positive number, it's not possible
-                }
-                if (i + 1 < n && A[i + 1] > 0) {
-                    int factor = -A[i] + 1;
+        for (int i = 0; i < n; i++) {
+            if (positive) {
+                if (A[i] <= 0) {
+                    int factor = 1 - A[i];
                     A[i] *= factor;
-                    A[i + 1] /= factor;
                     count++;
                 }
+                positive = false;
             } else {
-                if (i + 1 < n && A[i + 1] == 0) {
+                if (A[i] >= 0) {
                     int factor = A[i] + 1;
                     A[i] *= factor;
-                    A[i + 1] /= factor;
                     count++;
                 }
+                positive = true;
             }
         }
 
