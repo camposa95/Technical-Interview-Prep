70923435efc3f1ea647e0ba9ef421cebe334c8a2
camposa95 <95255953+camposa95@users.noreply.github.com>
2023-08-05 11:38:29 -0700

    w

--- src/main/java/TechnicalAssesments/Sentry/Sentry1.java
+++ src/main/java/TechnicalAssesments/Sentry/Sentry1.java
@@ -4,40 +4,30 @@ public class Sentry1 {
 
     public static String filterBadWords(String badWords, String message) {
         String[] badWordArray = badWords.split("\\s+");
-        String[] words = message.split("\\s+");
-        StringBuilder filteredMessage = new StringBuilder();
+        StringBuilder filteredMessage = new StringBuilder(message);
 
-        for (String word : words) {
-            boolean isBadWord = false;
+        for (String badWord : badWordArray) {
+            String regex = "\\b" + badWord.replace("*", "\\w*") + "\\b";
+            Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(filteredMessage);
 
-            for (String badWord : badWordArray) {
-                if (badWord.contains("*")) {
-                    // Handle bad words with wildcards (*)
-                    String regex = "\\b" + badWord.replace("*", "\\w*") + "\\b";
-                    Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-                    Matcher matcher = pattern.matcher(word);
-                    if (matcher.find()) {
-                        isBadWord = true;
-                        break;
-                    }
-                } else {
-                    // Handle exact bad words
-                    if (word.replaceAll("[^a-zA-Z]", "").equalsIgnoreCase(badWord)) {
-                        isBadWord = true;
-                        break;
-                    }
-                }
+            StringBuffer sb = new StringBuffer();
+            while (matcher.find()) {
+                String replacement = "*".repeat(matcher.group().length());
+                matcher.appendReplacement(sb, replacement);
             }
-
-            if (isBadWord) {
-                filteredMessage.append("*".repeat(word.length())).append(" ");
-            } else {
-                filteredMessage.append(word).append(" ");
-            }
+            matcher.appendTail(sb);
+            filteredMessage = sb;
         }
 
-        return filteredMessage.toString().trim();
+        return filteredMessage.toString();
     }
+
+
+
+
+
+
 
 
 
