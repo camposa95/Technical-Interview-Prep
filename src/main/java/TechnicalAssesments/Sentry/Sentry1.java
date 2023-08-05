acdca2e5338505f9b3ff923e3f2b685b3137c17c
camposa95 <camposa@rose-hulman.edu>
2023-08-05 11:16:54 -0700

    done with signature

---
+++ src/main/java/TechnicalAssesments/Sentry/Sentry1.java
@@ -0,0 +1,25 @@
+package TechnicalAssesments.Sentry;
+
+public class Sentry1 {
+    
+    public static String filterBadWords(String badWords, String message) {
+
+        // given badwords a whitespace separated string of bad words
+        // and message
+        // return the string filtered of all bad words by replacing them with *'s equal to the 
+        // length of the bad word
+
+        // Other considerations:
+        // bad words can have wildcards in them represented by a *
+        // for example given:
+        // badWords = "jerk* *lame*"
+        // message = "Stop it, you jerkwad! I remain blameless!"
+        // the output should be:
+        // "Stop it, you *******! I remain *********!"
+
+        // implementation here:
+
+        
+
+    }
+}
