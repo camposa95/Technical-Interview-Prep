4555f2a1581f62ca8e45e5f1b1b72a61b32e79c2
camposa95 <95255953+camposa95@users.noreply.github.com>
2023-08-05 11:31:15 -0700

    attempt 2

--- src/main/java/TechnicalAssesments/Sentry/Sentry1.java
+++ src/main/java/TechnicalAssesments/Sentry/Sentry1.java
@@ -1,10 +1,10 @@
 package TechnicalAssesments.Sentry;
 
 public class Sentry1 {
-    
+
     public static String filterBadWords(String badWords, String message) {
-        String[] badWordArray = badWords.split("\\s+"); // Split badWords by whitespace
-        String[] words = message.split("\\s+"); // Split the message into words by whitespace
+        String[] badWordArray = badWords.split("\\s+");
+        String[] words = message.split("\\s+");
         StringBuilder filteredMessage = new StringBuilder();
 
         for (String word : words) {
@@ -14,13 +14,15 @@ public class Sentry1 {
                 if (badWord.contains("*")) {
                     // Handle bad words with wildcards (*)
                     String regex = "\\b" + badWord.replace("*", "\\w*") + "\\b";
-                    if (word.matches(regex)) {
+                    Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+                    Matcher matcher = pattern.matcher(word);
+                    if (matcher.find()) {
                         isBadWord = true;
                         break;
                     }
                 } else {
                     // Handle exact bad words
-                    if (word.equals(badWord)) {
+                    if (word.equalsIgnoreCase(badWord)) {
                         isBadWord = true;
                         break;
                     }
@@ -28,16 +30,14 @@ public class Sentry1 {
             }
 
             if (isBadWord) {
-                // If word is a bad word, replace it with *'s equal to the length of the word
                 filteredMessage.append("*".repeat(word.length())).append(" ");
             } else {
-                // Otherwise, keep the original word
                 filteredMessage.append(word).append(" ");
             }
         }
 
-        // Remove the trailing whitespace and return the filtered message
         return filteredMessage.toString().trim();
     }
+
 
 }
