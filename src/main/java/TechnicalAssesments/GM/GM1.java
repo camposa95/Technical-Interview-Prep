f6ec489af8d3e0319168e7de8acbe1c470be6fe6
camposa95 <95255953+camposa95@users.noreply.github.com>
2023-08-05 12:19:49 -0700

    Update GM1.java

--- src/main/java/TechnicalAssesments/GM/GM1.java
+++ src/main/java/TechnicalAssesments/GM/GM1.java
@@ -2,28 +2,22 @@ package TechnicalAssesments.GM;
 
 public class GM1 {
     public int solution(int[] A) {
-        int n = A.length;
-        int count = 0;
-        boolean positive = true;
+        **/
+        that, given an array A, returns the minimum number of multiplications needed to create the described sequence, or −1 if it is not possible.
 
-        for (int i = 0; i < n; i++) {
-            if (positive) {
-                if (A[i] <= 0) {
-                    int factor = 1 - A[i];
-                    A[i] *= factor;
-                    count++;
-                }
-                positive = false;
-            } else {
-                if (A[i] >= 0) {
-                    int factor = A[i] + 1;
-                    A[i] *= factor;
-                    count++;
-                }
-                positive = true;
-            }
-        }
+Examples:
 
-        return count;
+For A = [1, 0, 3, 4, 5, 0, 6], one of the optimal solutions is to multiply 3 by −2, 4 by 0 and 6 by −1. After these changes, A will transform to [1, 0, −6, 0, 5, 0, −6]. The signs of the consecutive elements will be [+, 0, −, 0, +, 0, −], as required in the task statement. The function should return 3.
+
+For A = [7, 4, −3, 0, −5, 1, 0], it is not possible to convert the last zero into a negative number. The function should return −1.
+
+For A = [−5, 0, 3, 0], the function should return 2.
+
+Assume that:
+
+N is an integer within the range [1..100];
+each element of array A is an integer within the range [−20..20].
+In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.
+        */
     }
 }
