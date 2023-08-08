01739aaff1dcb6e6bccba1d778116bc066fcfb12
camposa95 <camposa@rose-hulman.edu>
2023-08-08 16:40:11 -0700

    signature

--- src/main/java/TechnicalAssesments/BelevedereTRading/BelevedereTrading1.java
+++ src/main/java/TechnicalAssesments/BelevedereTRading/BelevedereTrading1.java
@@ -1,5 +1,20 @@
 package TechnicalAssesments.BelevedereTRading;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 public class BelevedereTrading1 {
-    
+    /**
+     * Iterate through each line of input.
+     */
+    public static void main(String[] args) throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
+        BufferedReader in = new BufferedReader(reader);
+        String line;
+        while ((line = in.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
 }
