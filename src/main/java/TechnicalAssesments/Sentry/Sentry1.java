ccd3018d26a1fe5e93e4edf4176ad132c8eed8b7
camposa95 <camposa@rose-hulman.edu>
2023-08-09 18:08:42 -0700

    2

--- src/main/java/TechnicalAssesments/Sentry/Sentry1.java
+++ src/main/java/TechnicalAssesments/Sentry/Sentry1.java
@@ -8,47 +8,43 @@ public class Sentry1 {
     public static List<String> add_whitespace(String words, int line_char_limit) {
         String[] wordArray = words.split("\\s+");
         List<String> result = new ArrayList<>();
-        int currentLineLength = 0;
         StringBuilder currentLine = new StringBuilder();
 
         for (String word : wordArray) {
-            if (currentLineLength + currentLine.length() + word.length() <= line_char_limit) {
+            if (currentLine.length() + word.length() <= line_char_limit) {
                 if (currentLine.length() > 0) {
                     currentLine.append(" ");
-                    currentLineLength++;
                 }
                 currentLine.append(word);
-                currentLineLength += word.length();
             } else {
-                result.add(formatLine(currentLine.toString(), currentLineLength, line_char_limit));
+                result.add(formatLine(currentLine.toString(), line_char_limit));
                 currentLine = new StringBuilder(word);
-                currentLineLength = word.length();
             }
         }
 
         if (currentLine.length() > 0) {
-            result.add(formatLine(currentLine.toString(), currentLineLength, line_char_limit));
+            result.add(formatLine(currentLine.toString(), line_char_limit));
         }
 
         return result;
     }
 
-    private static String formatLine(String line, int currentLength, int line_char_limit) {
+    private static String formatLine(String line, int line_char_limit) {
         if (line.contains("-")) {
             int hyphenIndex = line.indexOf("-");
             String firstPart = line.substring(0, hyphenIndex + 1);
             String secondPart = line.substring(hyphenIndex + 1);
-
-            if (currentLength + secondPart.length() <= line_char_limit) {
+    
+            if (line.length() + secondPart.length() <= line_char_limit) {
                 return line;
             } else {
                 return firstPart.trim();
             }
         } else {
-            int extraWhitespace = line_char_limit - currentLength;
+            int extraWhitespace = line_char_limit - line.length();
             int spaceToAdd = line.contains(" ") ? extraWhitespace / (line.split(" ").length - 1) : 0;
             int remainder = line.contains(" ") ? extraWhitespace % (line.split(" ").length - 1) : 0;
-
+    
             StringBuilder formattedLine = new StringBuilder();
             String[] words = line.split(" ");
             for (int i = 0; i < words.length; i++) {
@@ -66,6 +62,6 @@ public class Sentry1 {
             return formattedLine.toString();
         }
     }
-
+    
 
 }
